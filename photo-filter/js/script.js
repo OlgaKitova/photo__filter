e50c3bc48d7ba5a5fa127fe059a474e015f9bfff
Olga Kitova <olga_and_kitova@bk.ru>
2021-03-17 17:01:50 +0500

    feat: add fullscreen function

--- photo-filter/js/script.js
+++ photo-filter/js/script.js
@@ -3,10 +3,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 const inputs = document.querySelectorAll('.total'),
       reset = document.querySelector('.btn-reset'),
-      loadBtn = document.querySelector('.btn-load--input'),
-      loadImg = document.querySelector('.loadImg');
+      loadBtn = document.querySelector('.btn-load'),
+      loadImg = document.querySelector('.loadImg'),
+      btnTotal = document.querySelectorAll('.btn');
 let result = document.querySelectorAll('.result');
 
+//Filter Function 
 function hundleUpdate() {
  switch(this.name) {
   case('blur'):
@@ -28,8 +30,10 @@ function hundleUpdate() {
 const suffix = this.dataset.sizing || '';
 document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
-
+//Function Reset Filters
 reset.addEventListener('click', () => {
+   btnTotal.forEach(btn => btn.classList.remove('btn-active'));
+   reset.classList.add('btn-active');
   result.forEach(resultInput => resultInput.value = 0);
   inputs.forEach(input => {
     input.value = 0;
@@ -45,17 +49,40 @@ reset.addEventListener('click', () => {
 
 inputs.forEach(input => input.addEventListener('change', hundleUpdate));
 
-loadBtn.addEventListener('change', () => {
-  console.log(loadBtn.value);
-  loadImg.src = loadBtn.value;
+//Function Load Image
+loadBtn.addEventListener('click', ()=> {
+  btnTotal.forEach(btn => btn.classList.remove('btn-active'));
+   loadBtn.classList.add('btn-active');
 })
+ 
+loadBtn.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    file = fileList[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+    loadImg.src = event.target.result;
+  });
+  reader.readAsDataURL(file);
+});
 
+//Fullscreen
 
+    const fullscreenBtn = document.querySelector('.fullscreen');
 
-
-
-
-
+    document.addEventListener('click', function(e) {
+      if(e.target == fullscreenBtn) {
+         toggleFullScreen();
+      }
+    });
+    function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
 
 
 
