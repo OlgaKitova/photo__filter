0317a6f68ece869346a88d03e21272cc86a6031a
Olga Kitova <olga_and_kitova@bk.ru>
2021-03-19 12:56:57 +0500

    feat: add canvas load an image and draw in a canvas

--- photo-filter/js/script.js
+++ photo-filter/js/script.js
@@ -5,11 +5,100 @@ const inputs = document.querySelectorAll('.total'),
       reset = document.querySelector('.btn-reset'),
       loadBtn = document.querySelector('.btn-load'),
       loadImg = document.querySelector('.loadImg'),
-      btnTotal = document.querySelectorAll('.btn');
+      btnTotal = document.querySelectorAll('.btn'),
+      btnSave = document.querySelector('.btn-save'),
+      btnNext = document.querySelector('.btn-next');
 let result = document.querySelectorAll('.result');
 
-//Filter Function 
-function hundleUpdate() {
+
+
+//Canvas
+const canvas = document.querySelector('canvas');
+let total = 0; 
+// Функция переключателя кнопки и изменения цвета при нажатии
+function activeSwitchButton(btns) {
+  btnTotal.forEach(btn => btn.classList.remove('btn-active'));
+   btns.classList.add('btn-active');
+}
+// При клике меняем активность 
+loadBtn.addEventListener('click', () => {
+  activeSwitchButton(loadBtn);
+});
+btnNext.addEventListener('click', () => {
+  activeSwitchButton(btnNext);
+});
+// Функция загрузки, отрисовки изображения  в канве и сохранения парметров
+function drawImage() {
+  const canvas = document.querySelector('canvas');
+  const img = new Image();  
+  img.setAttribute('crossOrigin', 'anonymous');
+  img.src = 'https://github.com/rolhttps://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+  //Загрузка изображения с компьютера
+  loadBtn.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    file = fileList[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+    img.src = event.target.result;
+  });
+  reader.readAsDataURL(file);
+});
+//Отрисовка изображения в канве
+  img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+// Изображению задаем значения из инпутов
+    inputs.forEach(input => {
+      let suffix = input.dataset.sizing;
+      input.addEventListener('input', (e) => {
+        ctx.filter = `${e.target.name}(${e.target.value}${suffix})`;
+         ctx.drawImage(img, 0, 0);
+         ctx.save();
+      });
+    });
+    ctx.drawImage(img, 0, 0);
+    ctx.save();
+    // Функция сохрания и скачивания изображения
+   const downloadImage = function(){
+    let link = document.createElement('a');
+    link.download = 'filename.jpg';
+    link.href = canvas.toDataURL();
+    if(total>0) {
+      link.click();
+      link.remove();
+    }
+}
+// Функция вызова скачивания с помощью кнопки
+btnSave.addEventListener('click', () => {
+  activeSwitchButton(btnSave);
+  total = 1;
+  downloadImage();
+});
+// Обнуление всех значений в канве 
+reset.addEventListener('click', () => {
+  inputs.forEach(input => {
+    let suffix = input.dataset.sizing;
+    ctx.filter = `${input.name}(0)`;
+    ctx.drawImage(img, 0, 0);
+    ctx.save();
+if(input.name == 'saturate') {
+      ctx.filter = `saturate(100${suffix})`;
+      ctx.drawImage(img, 0, 0);
+      ctx.save();
+    }
+    
+  });
+
+});
+}
+}
+// Вызов функции
+drawImage();
+// Меняем значения числовые
+inputs.forEach(input => input.addEventListener('change', hundleUpDate));
+
+function hundleUpDate() {
  switch(this.name) {
   case('blur'):
   result[0].value = this.value;
@@ -27,13 +116,10 @@ function hundleUpdate() {
   result[4].value = this.value;
   break;
  }
-const suffix = this.dataset.sizing || '';
-document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
-//Function Reset Filters
+// Меняем при сбросе числовые значения у инпутов
 reset.addEventListener('click', () => {
-   btnTotal.forEach(btn => btn.classList.remove('btn-active'));
-   reset.classList.add('btn-active');
+   activeSwitchButton(reset);
   result.forEach(resultInput => resultInput.value = 0);
   inputs.forEach(input => {
     input.value = 0;
@@ -43,26 +129,7 @@ reset.addEventListener('click', () => {
     }
     const suffix = input.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix);
-  
   });
-});
-
-inputs.forEach(input => input.addEventListener('change', hundleUpdate));
-
-//Function Load Image
-loadBtn.addEventListener('click', ()=> {
-  btnTotal.forEach(btn => btn.classList.remove('btn-active'));
-   loadBtn.classList.add('btn-active');
-})
- 
-loadBtn.addEventListener('change', (event) => {
-    const fileList = event.target.files;
-    file = fileList[0];
-    const reader = new FileReader();
-    reader.addEventListener('load', (event) => {
-    loadImg.src = event.target.result;
-  });
-  reader.readAsDataURL(file);
 });
 
 //Fullscreen
@@ -83,13 +150,6 @@ loadBtn.addEventListener('change', (event) => {
     }
   }
 }
-
-
-
-
-
-
-
 
 
 
