12091ef32ed1014bfc944fffc4232850fd954eee
Olga Kitova <olga_and_kitova@bk.ru>
2021-03-20 10:33:28 +0500

    feat: add screenshot image

--- photo-filter/js/script.js
+++ photo-filter/js/script.js
@@ -14,6 +14,8 @@ let result = document.querySelectorAll('.result');
 
 //Canvas
 const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext("2d");
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 let total = 0; 
 
 // База данных фотографий
@@ -24,6 +26,7 @@ const imagesDay = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07
 const imagesEvening = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 const imagesNight = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
+let blur, invert, sepia, saturate, hue;
 
 const dateNow = new Date();
 const dateHours = dateNow.getHours();
@@ -37,6 +40,7 @@ function activeSwitchButton(btns) {
 loadBtn.addEventListener('click', () => {
   activeSwitchButton(loadBtn);
 });
+
 // Функция загрузки, отрисовки изображения  в канве и сохранения парметров
 function drawImage() {
   const canvas = document.querySelector('canvas');
@@ -46,34 +50,41 @@ function drawImage() {
 changeTimeImg();
 function changeTimeImg() {
   let imageSrc, index;
-    if(dateHours > 6 && dateHours < 12) {
+    if(dateHours >= 6 && dateHours < 12) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
      index = i % imagesMorning.length;
      imageSrc = base + 'morning/' + imagesMorning[index];
     }
-    if(dateHours > 12 && dateHours < 18) {
+    if(dateHours >= 12 && dateHours < 18) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
       index = i % imagesDay.length;
      imageSrc = base + 'day/' + imagesDay[index];
     }
-    if(dateHours > 18 && dateHours < 24) {
+    if(dateHours >= 18 && dateHours < 24) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
       index = i % imagesEvening.length;
      imageSrc = base + 'evening/' + imagesEvening[index];
     }
-    if(dateHours > 24 && dateHours < 6) {
+    if(dateHours >= 0 && dateHours < 6) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
       index = i % imagesNight.length;
      imageSrc = base + 'night/' + imagesNight[index];
     }
   i++;
   btnNext.disabled = true;
   setTimeout(function() { btnNext.disabled = false }, 1000);
+ ctx.clearRect(0, 0, canvas.width, canvas.height);
   img.src = imageSrc;
 }
 // Кнопка по переключению изображений
 btnNext.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
    activeSwitchButton(btnNext);
     changeTimeImg();
   });
   //Загрузка изображения с компьютера
   loadBtn.addEventListener('change', (event) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     const fileList = event.target.files;
     file = fileList[0];
     const reader = new FileReader();
@@ -87,53 +98,65 @@ btnNext.addEventListener('click', () => {
     canvas.width = img.width;
     canvas.height = img.height;
     const ctx = canvas.getContext("2d");
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 // Изображению задаем значения из инпутов
     inputs.forEach(input => {
       let suffix = input.dataset.sizing;
       input.addEventListener('input', (e) => {
-        ctx.filter = `${e.target.name}(${e.target.value}${suffix})`;
-         ctx.drawImage(img, 0, 0);
-         ctx.save();
+  if(e.target.name == 'blur')  blur = `${e.target.name}(${e.target.value}${suffix})`;
+  if(e.target.name == 'invert') invert = `${e.target.name}(${e.target.value}${suffix})`;
+  if(e.target.name == 'sepia') sepia = `${e.target.name}(${e.target.value}${suffix})`;
+  if(e.target.name == 'saturate') saturate = `${e.target.name}(${e.target.value}${suffix})`;
+  if(e.target.name == 'hue') hue = `hue-rotate(${e.target.value}${suffix})`;
+   ctx.filter = `${blur || ''} ${invert || ''} ${sepia || ''} ${saturate || ''} ${hue || ''}`;
+  ctx.drawImage(img, 0, 0);
       });
     });
     ctx.drawImage(img, 0, 0);
-    ctx.save();
-    // Функция сохрания и скачивания изображения
-   const downloadImage = function(){
-    let link = document.createElement('a');
-    link.download = 'filename.jpg';
-    link.href = canvas.toDataURL();
-    if(total>0) {
-      link.click();
-      link.remove();
-    }
-}
-// Функция вызова скачивания с помощью кнопки
-btnSave.addEventListener('click', () => {
-  activeSwitchButton(btnSave);
-  total = 1;
-  downloadImage();
-});
 // Обнуление всех значений в канве 
 reset.addEventListener('click', () => {
   inputs.forEach(input => {
     let suffix = input.dataset.sizing;
-    ctx.filter = `${input.name}(0)`;
+    blur = 0; 
+    invert = 0;
+    sepia = 0;
+    hue = 0;
+    ctx.filter = `${blur || ''} ${invert || ''} ${sepia || ''} ${saturate || ''} ${hue || ''}`;
     ctx.drawImage(img, 0, 0);
-    ctx.save();
 if(input.name == 'saturate') {
       ctx.filter = `saturate(100${suffix})`;
       ctx.drawImage(img, 0, 0);
-      ctx.save();
-    }
-    
+    } 
   });
-
 });
-}
+};
 }
 // Вызов функции
 drawImage();
+
+
+    // Функция сохрания и скачивания изображения
+
+   const downloadImage = function(url){
+    let link = document.createElement('a');
+    link.download = 'image.jpg';
+    link.href = url;
+     if(total>0) {
+     link.click();
+     link.remove();
+     total-=1;
+}
+link.remove(); 
+}
+// Функция вызова скачивания с помощью кнопки
+btnSave.addEventListener('click', () => {
+  activeSwitchButton(btnSave);
+  total = 1;
+  downloadImage(canvas.toDataURL());
+});
+
+
+
 // Меняем значения числовые
 inputs.forEach(input => input.addEventListener('change', hundleUpDate));
 
